fec413c9f0d75853f471c97b729cf8ffa9d73bd2
David Sidrane <david_s5@nscdg.com>
2017-07-14 14:00:51 -1000

    px4fmu-v2:board init restructuring VDD_3V3_SENSORS and PWM
       Insure a 0.0 voltage initial condition on VDD_3V3_SENSORS
       By starting the GPIO_VDD_3V3_SENSORS_EN, low and deferring
       the GPIO init of the slave selects and drdy signals until
       board_app_initialize. We get ~ 180 ms of power off time
       with 0.00 voltage applied to the sensors.
    
       Add board_on_reset to insure FMU PWM is clamped on reset
       and held off for > 400 ms to delay the 3.1 ms pules that
       is due to the delay from chip reset to setting the GPIO
       pins.

--- src/drivers/boards/px4fmu-v2/px4fmu2_init.c
+++ src/drivers/boards/px4fmu-v2/px4fmu2_init.c
@@ -145,6 +145,47 @@ __EXPORT void board_peripheral_reset(int ms)
 }
 
 /************************************************************************************
+  * Name: board_on_reset
+ *
+ * Description:
+ * Optionally provided function called on entry to board_system_reset
+ * It should perform any house keeping prior to the rest.
+ *
+ * status - 1 if resetting to boot loader
+ *          0 if just resetting
+ *
+ ************************************************************************************/
+__EXPORT void board_on_reset(int status)
+{
+	UNUSED(status);
+	/* configure the GPIO pins to outputs and keep them low */
+
+	stm32_configgpio(GPIO_GPIO0_OUTPUT);
+	stm32_configgpio(GPIO_GPIO1_OUTPUT);
+	stm32_configgpio(GPIO_GPIO2_OUTPUT);
+	stm32_configgpio(GPIO_GPIO3_OUTPUT);
+	stm32_configgpio(GPIO_GPIO4_OUTPUT);
+	stm32_configgpio(GPIO_GPIO5_OUTPUT);
+
+	/* On resets invoked from system (not boot) insure we establish a low
+	 * output state (discharge the pins) on PWM pins before they become inputs.
+	 *
+	 * We also delay the onset of the that 3.1 Ms pulse as boot. This has
+	 * triggered some ESC to spin. By adding this delay here the reset
+	 * is pushed out > 400 ms. So the ESC PWM input can not mistake
+	 * the 3.1 Ms pulse as a valid PWM command.
+	 *
+	 * fixme:Establish in upstream NuttX an CONFIG_IO_INIT_STATE to
+	 * the initialize the IO lines in the clock config.
+	 *
+	 */
+
+	if (status >= 0) {
+		up_mdelay(400);
+	}
+}
+
+/************************************************************************************
  * Name: stm32_boardinitialize
  *
  * Description:
@@ -157,6 +198,14 @@ __EXPORT void board_peripheral_reset(int ms)
 __EXPORT void
 stm32_boardinitialize(void)
 {
+	board_on_reset(-1);
+
+	/* configure LEDs */
+	board_autoled_initialize();
+
+	/* Start with Power off */
+	stm32_configgpio(GPIO_VDD_3V3_SENSORS_EN);
+
 	/* configure ADC pins */
 
 	stm32_configgpio(GPIO_ADC1_IN2);	/* BATT_VOLTAGE_SENS */
@@ -168,25 +217,20 @@ stm32_boardinitialize(void)
 
 	/* configure power supply control/sense pins */
 	stm32_configgpio(GPIO_VDD_5V_PERIPH_EN);
-	stm32_configgpio(GPIO_VDD_3V3_SENSORS_EN);
 	stm32_configgpio(GPIO_VDD_BRICK_VALID);
 	stm32_configgpio(GPIO_VDD_SERVO_VALID);
+	stm32_configgpio(GPIO_VDD_USB_VALID);
 	stm32_configgpio(GPIO_VDD_5V_HIPOWER_OC);
 	stm32_configgpio(GPIO_VDD_5V_PERIPH_OC);
 
-	/* configure the GPIO pins to outputs and keep them low */
-	stm32_configgpio(GPIO_GPIO0_OUTPUT);
-	stm32_configgpio(GPIO_GPIO1_OUTPUT);
-	stm32_configgpio(GPIO_GPIO2_OUTPUT);
-	stm32_configgpio(GPIO_GPIO3_OUTPUT);
-	stm32_configgpio(GPIO_GPIO4_OUTPUT);
-	stm32_configgpio(GPIO_GPIO5_OUTPUT);
 
-	/* configure SPI interfaces */
-	stm32_spiinitialize();
+	/* configure SPI interfaces
+	 * is deferred to board_app_initialize
+	 * to delay the sensor power up with
+	 * out adding a delay
+	 */
 
-	/* configure LEDs */
-	board_autoled_initialize();
+
 }
 
 /****************************************************************************
@@ -234,6 +278,15 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 #else
 #  error platform is dependent on c++ both CONFIG_HAVE_CXX and CONFIG_HAVE_CXXINITIALIZE must be defined.
 #endif
+
+	/* Bring up the Sensor power */
+
+	stm32_gpiowrite(GPIO_VDD_3V3_SENSORS_EN, 1);
+
+	/* Now it is ok to drive the pins high
+	 * so configure SPI CPIO */
+
+	stm32_spiinitialize();
 
 	/* configure the high-resolution time/callout interface */
 	hrt_init();
